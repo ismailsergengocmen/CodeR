bc6bb3f8713aee82b565af15dcb0ba34b535e2a2
Sule03 <sulenuryilmaz123@gmail.com>
2022-04-22 13:52:18 +0300

    Added some classes

---
+++ backend/src/main/java/com/backend/backend/entity/Interview.java
@@ -0,0 +1,19 @@
+package com.backend.backend.entity;
+
+public class Interview {
+private int interviewid;
+private String title;
+
+public Interview(int interviewid, String title){
+    this.interviewid = interviewid;
+    this.title = title;
+}
+
+
+
+
+
+
+
+
+}
