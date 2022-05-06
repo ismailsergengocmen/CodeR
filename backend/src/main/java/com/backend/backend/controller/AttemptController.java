3e78cf55c7c3601d17ad089db1a40f0c1175b531
Ahmed Salih Cezayir <63608956+AhmedSalihCezayir@users.noreply.github.com>
2022-05-06 19:04:21 +0300

    Attempt endpoints are added

---
+++ backend/src/main/java/com/backend/backend/controller/AttemptController.java
@@ -0,0 +1,38 @@
+package com.backend.backend.controller;
+
+import com.backend.backend.entity.CodingAttempt;
+import com.backend.backend.entity.NonCodingAttempt;
+import com.backend.backend.repository.AttemptRepository;
+import com.backend.backend.util.SubmitResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("api/v1/attempt")
+public class AttemptController {
+    @Autowired
+    AttemptRepository attemptRepository;
+
+    @PostMapping("submit/challenge")
+    public SubmitResult submitChallenge(@RequestBody CodingAttempt codingAttempt) {
+        return attemptRepository.submitChallenge(codingAttempt);
+    }
+
+    @PostMapping("submit/noncoding")
+    public Boolean submitNonCodingQuestion(@RequestBody NonCodingAttempt nonCodingAttempt) {
+        return attemptRepository.submitNonCodingQuestion(nonCodingAttempt);
+    }
+
+    @GetMapping("past/challenge/{question_id}")
+    public List<CodingAttempt> getPastChallengeAttempt(@PathVariable Integer question_id) {
+        return attemptRepository.getPastChallengeAttempt(question_id);
+    }
+
+    @GetMapping("past/noncoding/{question_id}")
+    public List<NonCodingAttempt> getPastNonCodingQuestionAttempts(@PathVariable Integer question_id) {
+        return attemptRepository.getPastNonCodingQuestionAttempts(question_id);
+    }
+}
