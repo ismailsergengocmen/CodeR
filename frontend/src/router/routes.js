c662698ca086bc61e3188a0b39aa9729bc192e90
İsmail Sergen Göçmen <ismail.gocmen@ug.bilkent.edu.tr>
2022-05-16 23:15:49 +0300

    create Inter page created and contest page updated

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,98 +1,42 @@
-import Login from "../pages/auth/Login.vue";
-import Register from "../pages/auth/Register.vue";
-import ChallengeFilter from "../components/challenge/ChallengeFilter.vue";
-import ChallengeSpecificPage from "../pages/challenge/ChallengeSpecificPage.vue";
-import Error404 from "../pages/Error404";
+import Login from "../pages/auth/Login.vue"
+import Register from "../pages/auth/Register.vue"
+import ChallengeFilter from "../components/challenge/ChallengeFilter.vue"
+import CreateChallengePage from "../pages/challenge/CreateChallengePage.vue"
+import CreateContestPage from "../pages/contest/CreateContestPage.vue"
+import CreateInterviewPage from "../pages/interview/CreateInterviewPage.vue"
+import Error404 from "../pages/Error404"
 
 const routes = [
   {
-    path: "/register",
-    component: Register,
+    path: '/register',
+    component: Register
   },
   {
-    path: "/",
-    component: Login,
+    path: '/',
+    component: Login
   },
   {
-    path: "/filter",
-    component: ChallengeFilter,
+    path: '/filter',
+    component: ChallengeFilter
   },
   {
-    path: "/challenge/:course_id",
-    component: ChallengeSpecificPage,
-    props: true,
+    path: '/createQuestion',
+    component: CreateChallengePage
   },
-  //   children: [
-  //     {
-  //       path: 'home',
-  //       name: 'Home',
-  //       component: HomePage,
-  //       meta: {
-  //         haveAccess: ['JobSeeker', 'Company', 'Editor']
-  //       }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/~',
-  //   component: BaseLayoutTab,
-  //   children: [
-  //     {
-  //       path: 'challenges',
-  //       name: 'Challenges',
-  //       component: ChallengesPage,
-  //       meta: {
-  //         haveAccess: ['JobSeeker']
-  //       }
-  //     },
-  //     {
-  //       path: 'challenges/:id',
-  //       name: 'ChallengeSpecificPage',
-  //       component: CourseSpecificPage,
-  //       props: true,
-  //       meta: {
-  //         haveAccess: ['JobSeeker']
-  //       }
-  //     },
-  //     {
-  //       path: 'contests',
-  //       name: 'Contests',
-  //       component: ContestsPage,
-  //       meta: {
-  //         haveAccess: ['JobSeeker']
-  //       }
-  //     },
-  //     {
-  //       path: 'contests/:id',
-  //       name: 'ContestSpecificPage',
-  //       component: ContestSpecificPage,
-  //       props: true,
-  //       meta: {
-  //         haveAccess: ['JobSeeker']
-  //       }
-  //     }
-  //   ]
-  // },
-  // {
-  //   path: '/auth',
-  //   component: BaseIndex,
-  //   children: [
-  //     {
-  //       path: 'login',
-  //       name: 'Login',
-  //       component: Login
-  //     },
-  //     {
-  //       path: 'register',
-  //       name: 'Register',
-  //       component: Register
-  //     }
-  //   ]
-  // },
   {
-    path: "/:catchAll(.*)*",
-    name: "Error404",
-    component: Error404,
+    path: '/createContest',
+    component: CreateContestPage
   },
-];
-export default routes;
+  {
+    path: '/createInterview',
+    component: CreateInterviewPage
+  },
+  
+  {
+    path: '/:catchAll(.)',
+    name: 'Error404',
+    component: Error404
+  }
+]
+
+export default routes
