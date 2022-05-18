fc2ef624a034a6dc2f2acfa24967684a214ea1b1
Ahmed Salih Cezayir <63608956+AhmedSalihCezayir@users.noreply.github.com>
2022-05-18 05:42:41 +0300

    yolo

--- frontend/src/pages/interview/JobSeekerInInterviewPage.vue
+++ frontend/src/pages/interview/JobSeekerInInterviewPage.vue
@@ -1,92 +1,92 @@
 <template>
-<div>
+  <div>
     <div>
-        <q-btn label="Go to LeaderBoard" class="q-pa-sm"/>  
+      <q-btn label="Go to LeaderBoard" class="q-pa-sm" />
     </div>
     <div class="full-height">
-        <div class="fixed-top-center q-pa-md">
-            <q-banner class="bg-grey text-white" dense>
-                <span class="row justify-center"> {{interviewName}} </span>
-            </q-banner>
+      <div class="fixed-top-center q-pa-md">
+        <q-banner class="bg-grey text-white" dense>
+          <span class="row justify-center"> {{ interviewName }} </span>
+        </q-banner>
+      </div>
+      <div class="row bordered justify-around q-pa-sm">
+        <div>
+          <span class="label bg-white text-black">Start Date</span>
+          <span class="row justify-center"> {{ interviewDate }} </span>
         </div>
-        <div class="row bordered justify-around q-pa-sm">
-            <div>
-                <span class="label bg-white text-black">Start Date</span>
-                <span class="row justify-center"> {{interviewDate}} </span>
+      </div>
+      <div class="row justify-content">
+        <span class="label bg-white text-black">Questions</span>
+        <q-scroll-area visible style="height: 300px" class="col">
+          <div
+            v-for="question in interviewQuestions"
+            :key="question.question_id"
+            class="q-pa-sm"
+          >
+            <div class="row justify-content">
+              <div>
+                <span class="label bg-white text-black">
+                  {{ question.question_title }}
+                </span>
+              </div>
+              <div>
+                <q-separator vertical inset class="q-pa-md" />
+              </div>
+              <div>
+                <q-btn
+                  label="Begin"
+                  :to="`/~/challenge/${question.question_id}`"
+                  class="q-pa-sm"
+                />
+              </div>
             </div>
-            <div>
-                <span class="label bg-white text-black">Categories</span>
-                <span class="row justify-center"> {{contestCategories}} </span>
-            </div>
-        </div>
-        <div class="row justify-content">
-            <span class="label bg-white text-black">Questions</span>
-            <q-scroll-area
-                visible
-                style="height: 300px;"
-                class="col"
-                ref="firstRef"
-                >
-                <div v-for="question in interviewQuestions" :key="question.question_id" class="q-pa-sm">
-                    <div class="row justify-content">
-                        <div>
-                            <span class="label bg-white text-black"> {{ question.question_title }} </span>
-                        </div>
-                        <div>
-                            <q-separator vertical inset class="q-pa-md"/>
-                        </div>
-                        <div>
-                            <q-btn label="Begin" :to="`/~/challenge/${question.question_id}`" class="q-pa-sm"/>  
-                        </div>
-                    </div>
-                </div> 
-            </q-scroll-area>
-        </div>
+          </div>
+        </q-scroll-area>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 
 <script>
-import { ref, onBeforeMount } from '@vue/runtime-core'
-import { useRouter } from "vue-router"
-import { useQuasar } from "quasar"
-import { api } from "../../boot/axios"
+import { ref, onBeforeMount } from "@vue/runtime-core";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+import { api } from "../../boot/axios";
 
 export default {
-    name: "JobSeekerInContestPage",
-    props: ["id"],
+  name: "JobSeekerInContestPage",
+  props: ["id"],
 
-    setup(props) {
-        const $q = useQuasar();
-        const router = useRouter();
-        
-        const interviewName = ref('')
-        const interviewDate = ref('')
-        const interviewQuestions = ref([])
+  setup(props) {
+    const $q = useQuasar();
+    const router = useRouter();
 
-        onBeforeMount(() => {
-            api.get(`api/v1/interview/${props.id}`).then((response) => {
-                if(response.data){
-                    interviewName.value = response.data.title
-                    interviewDate.value = response.data.start_date
-                    interviewQuestions.value = response.data.contest_questions
-                }
-                else{
-                    $q.notify({
-                        position:"top",
-                        color:"negative",
-                        message:"Selected contest can not be found"
-                    })
-                }
-            })
-        })
+    const interviewName = ref("");
+    const interviewDate = ref("");
+    const interviewQuestions = ref([]);
 
-        return {
-            interviewName,
-            interviewDate,
-            interviewQuestions
+    onBeforeMount(() => {
+      api.get(`api/v1/interview/${props.id}`).then((response) => {
+        if (response.data) {
+          interviewName.value = response.data.title;
+          interviewDate.value = response.data.start_date;
+          interviewQuestions.value = response.data.interview_questions;
+        } else {
+          $q.notify({
+            position: "top",
+            color: "negative",
+            message: "Selected contest can not be found",
+          });
         }
-    }
-}
+      });
+    });
+
+    return {
+      interviewName,
+      interviewDate,
+      interviewQuestions,
+    };
+  },
+};
 </script>
--- frontend/src/pages/interview/JobSeekerInterviewPage.vue
+++ frontend/src/pages/interview/JobSeekerInterviewPage.vue
@@ -1,113 +1,120 @@
 <template>
-    <div>
-        <div class= "q-pa-md">
-            <q-banner dense label="Challenges" class="text-white bg-grey">
-                <span class="row justify-center"> My Interviews </span>
-            </q-banner>
-        </div>
-        <div>
-            <q-input
-                v-model="currentName"
-                debounce="1500"
-                outlined
-                dense
-                placeholder='Search'
-                color="secondary"
-            >
-                <template v-slot:prepend>
-                    <q-icon name="search" />
-                </template>
-            </q-input>
-        </div>
-        <div>
-            <q-scroll-area
-                visible
-                style="height: 300px;"
-                class="col"
-                ref="firstRef"
-                >
-                <div v-for="interview in interviews" :key="interview.interview_id" class="q-pa-sm">
-                    <div class="row justify-content">
-                        <div>
-                            <span class="label bg-white text-black"> {{ interview.title }} </span>
-                        </div>
-                        <div>
-                            <q-separator vertical inset />
-                        </div>
-                        <div>
-                            <span class="label bg-white text-black"> {{ interview.start_date }} </span>
-                        </div>
-                        <div>
-                            <q-separator vertical inset />
-                        </div>
-                        <div>
-                            <q-btn label="Begin" :to="`${interview.interview_id}`"/>  
-                        </div>
-                    </div>
-                </div> 
-            </q-scroll-area>
-        </div>
+  <div>
+    <div class="q-pa-md">
+      <q-banner dense label="Challenges" class="text-white bg-grey">
+        <span class="row justify-center"> My Interviews </span>
+      </q-banner>
     </div>
+    <div>
+      <q-input
+        v-model="currentName"
+        debounce="1500"
+        outlined
+        dense
+        placeholder="Search"
+        color="secondary"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </div>
+    <div>
+      <q-scroll-area visible style="height: 300px" class="col" ref="firstRef">
+        <div
+          v-for="interview in interviews"
+          :key="interview.interview_id"
+          class="q-pa-sm"
+        >
+          <div class="row justify-content">
+            <div>
+              <span class="label bg-white text-black">
+                {{ interview.title }}
+              </span>
+            </div>
+            <div>
+              <q-separator vertical inset />
+            </div>
+            <div>
+              <span class="label bg-white text-black">
+                {{ interview.start_date }}
+              </span>
+            </div>
+            <div>
+              <q-separator vertical inset />
+            </div>
+            <div>
+              <q-btn
+                label="Begin"
+                :to="`/~/interview/${interview.interview_id}`"
+              />
+            </div>
+          </div>
+        </div>
+      </q-scroll-area>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, onBeforeMount, watch } from "vue"
-import { useRouter } from "vue-router"
-import { useQuasar } from "quasar"
-import { api } from "../../boot/axios"
+import { ref, onBeforeMount, watch } from "vue";
+import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
+import { api } from "../../boot/axios";
 
 export default {
-    name: "JobSeekerInterviewPage",
+  name: "JobSeekerInterviewPage",
 
-    setup() {
-        const $q = useQuasar();
-        const router = useRouter();
+  setup() {
+    const $q = useQuasar();
+    const router = useRouter();
 
-        const currentName = ref("");
-        const interviews = ref([])
+    const currentName = ref("");
+    const interviews = ref([]);
 
-        onBeforeMount(() => {
-            const user_id = localStorage.getItem("currentUserID")
-            api.get(`api/v1/interview/all?user_id=${user_id}`).then((response) => {
-                if(!response.data){
-                    $q.notify({
-                    position:"top",
-                    color:"negative",
-                    message:"Something wrong"
-                    })
-                }
-                else{
-                    interviews.value = response.data
-                }
-            })
-        })
-
-
-        watch(currentName, (newName) => {
-            search(newName)
-        })
-
-        const search = async (newName) => {
-            const user_id = localStorage.getItem("currentUserID")
-            api.get(`api/v1/interview/all?user_id=${user_id}&interview_name=${newName}`).then((response) => {
-                if(!response.data){
-                    $q.notify({
-                    position:"top",
-                    color:"negative",
-                    message:"Something wrong"
-                    })
-                }
-                else{
-                    interviews.value = response.data
-                }
-            })
+    onBeforeMount(() => {
+      const user_id = localStorage.getItem("currentUserID");
+      api.get(`api/v1/interview/all?user_id=${user_id}`).then((response) => {
+        if (!response.data) {
+          $q.notify({
+            position: "top",
+            color: "negative",
+            message: "Something wrong",
+          });
+        } else {
+          interviews.value = response.data;
         }
+      });
+    });
 
-        return {
-            currentName,
-            interviews,
-            search
-        }
-    },
-}
+    watch(currentName, (newName) => {
+      search(newName);
+    });
+
+    const search = async (newName) => {
+      const user_id = localStorage.getItem("currentUserID");
+      api
+        .get(
+          `api/v1/interview/all?user_id=${user_id}&interview_name=${newName}`
+        )
+        .then((response) => {
+          if (!response.data) {
+            $q.notify({
+              position: "top",
+              color: "negative",
+              message: "Something wrong",
+            });
+          } else {
+            interviews.value = response.data;
+          }
+        });
+    };
+
+    return {
+      currentName,
+      interviews,
+      search,
+    };
+  },
+};
 </script>
